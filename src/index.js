0bd27431784a868bb23d6843ba7b659a597778ec
Ian McMahon <imcmahon87@yahoo.com>
2022-09-30 20:05:55 -0700

    Started index.js with mobile header

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, Link, useLocation } from 'react-router-dom';
+import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import menuButton from './assets/images/menu-button-mobile.png';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  const location = useLocation();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <div className="wrapper">
+      <div className="header-mobile">
+        <Link to="/">
+          <h1>BEGGARS CANYON</h1>
+        </Link>
+        <div className="menu-mobile">
+          <Link to="/shows">Upcoming Shows</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <img className="menu-button-mobile" src={menuButton} />
+      </div>
+    </div>
+  );
+}
+
+const Root = () => <BrowserRouter><App /></BrowserRouter>;
+render(<Root />, document.getElementById('root'));
