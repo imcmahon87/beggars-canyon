30aad7cae72417476171a4e2fc3e7063fb63df96
Ian McMahon <imcmahon87@yahoo.com>
2022-10-08 15:42:04 -0700

    Solidify route animation and menu JavaScript

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import Shows from './components/Shows';
 function App() {
   const location = useLocation();
 
+  // Handle mobile menu accordion
   function mobileMenuPress() {
     var x = document.getElementById('menu-mobile');
     if (x.style.display === 'block') {
@@ -19,23 +20,53 @@ function App() {
     }
   }
 
+  // Minimize mobile menu when clicking outside of menu
+  window.onload = () => {
+    let menuArea = document.getElementById('header-mobile');
+    let mouseMenuArea;
+    menuArea.addEventListener('mouseover', () => {
+        mouseMenuArea = true;
+        console.log('in area');
+    });
+    menuArea.addEventListener('mouseleave', () => {
+        mouseMenuArea = false;
+        console.log('out of area');
+    });
+    document.addEventListener('click', () => {
+        if (mouseMenuArea === false) {
+            document.getElementById('menu-mobile').style.display = 'none';
+        }
+      
+    // Scroll to top when clicking a link (for animation purposes)
+    const menuLinks = document.getElementsByClassName('menu-link');
+    for (let i = 0; menuLinks.length; i++) {
+      menuLinks[i].addEventListener('click', () => {
+        document.getElementById('menu-mobile').style.display = 'none';
+        window.scrollTo(0, 0);
+      });
+    }
+  });
+
+
+};
+
   return (
     <div className="wrapper">
       <div id="header-mobile">
-        <Link to="/" style={{ display: 'inline-block' }}>
+        <Link to="/" className="menu-link" style={{ display: 'inline-block' }}>
           <h1>BEGGARS CANYON</h1>
         </Link>
         <div id="menu-mobile">
-          <Link to="/shows">Upcoming Shows</Link>
-          <Link to="/gallery">Gallery</Link>
-          <Link to="/contact">Contact</Link>
+          <Link to="/shows" className="menu-link">Upcoming Shows</Link>
+          <Link to="/gallery" className="menu-link">Gallery</Link>
+          <Link to="/contact" className="menu-link">Contact</Link>
         </div>
         <img  id="menu-button-mobile"
               src={menuButton}
               onClick={mobileMenuPress} />
       </div>
       <SwitchTransition>
-        <CSSTransition key={location.key} classNames="slide" timeout={1500}>
+        <CSSTransition key={location.key} classNames="slide" timeout={1000}>
           <Routes location={location}>
             <Route path="/" element={<Main />} />
             <Route path="/shows" element={<Shows />} />
